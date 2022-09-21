3429e1313cf31bd3f7c0d2289536ee72b010e7e6
reibot <113862420+reibot@users.noreply.github.com>
2022-09-21 06:01:20 +0000

    Create azusa-setup.sh

---
+++ azusa-setup.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#configure configs
+git clone https://github.com/reibot/reibot-arch
+rm ~/.config/qtile/config.py
+cp ~/reibot-arch/azusa/config.py ~/.config/qtile/
+rm ~/.config/qtile/sxhkdrc
+cp ~/reibot-arch/azusa/sxhkdrc ~/.config/qtile/
+rm ~/.config/qtile/scripts/autostart.sh
+cp ~/reibot-arch/azusa/autostart.sh ~/.config/qtile/scripts/
