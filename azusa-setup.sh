c26aa3600dfe9a0ab532bb0b75542685f6d0fa3c
reibot <113862420+reibot@users.noreply.github.com>
2022-09-21 06:25:54 +0000

    Update azusa-setup.sh

--- azusa-setup.sh
+++ azusa-setup.sh
@@ -4,8 +4,8 @@
 git clone https://github.com/reibot/reibot-arch
 rm /home/rei/.config/qtile/config.py
 cp /home/rei/reibot-arch/azusa/config.py /home/rei/.config/qtile/
-rm /home/rei/.config/qtile/sxhkdrc
-cp /home/rei/reibot-arch/reibot-arch/azusa/sxhkdrc /home/rei/.config/qtile/
+rm /home/rei/.config/qtile/sxhkd/sxhkdrc
+cp /home/rei/reibot-arch/reibot-arch/azusa/sxhkdrc /home/rei/.config/qtile/sxhkd
 rm /home/rei/.config/qtile/scripts/autostart.sh
 cp /home/rei/reibot-arch/azusa/autostart.sh /home/rei/.config/qtile/scripts/
-cp /home/reibot-arch/azusa/azusa.jpg /usr/share/backgrounds/arcolinux/
+cp /home/rei/reibot-arch/azusa/azusa.jpg /usr/share/backgrounds/arcolinux/
